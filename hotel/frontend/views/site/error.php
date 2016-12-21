6ba0d8759f6e4b777e0ce3f7a771879542bccaed
troshinana <troshns@mail.ru>
2016-12-21 19:35:10 +0700

    Update error.php

--- hotel/frontend/views/site/error.php
+++ hotel/frontend/views/site/error.php
@@ -7,21 +7,11 @@
 
 use yii\helpers\Html;
 
-$this->title = $name;
+$this->title = 'Ошибка';
 ?>
 <div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <h1>Ошибка №<?= $exception->statusCode?></h1>
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
